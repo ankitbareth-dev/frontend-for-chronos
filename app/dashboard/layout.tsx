2087b804d1b563ea8a2704e686d35ae3fe18ebe4
ankitbareth-dev <ankitbareth.dev@gmail.com>
2025-11-28 17:42:43 +0530

    revert the layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,33 +1,10 @@
-"use client";
-
-import { useState } from "react";
-import Sidebar from "./components/Sidebar/Sidebar";
 import Topbar from "./components/Topbar/Topbar";
 
-export default function DashboardLayout({ children }: any) {
-  const [isCollapsed, setIsCollapsed] = useState(false);
-
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* TOPBAR ALWAYS ON TOP */}
       <Topbar />
-
-      <div className="layoutWrapper">
-        {/* SIDEBAR BELOW TOPBAR */}
-        <Sidebar onCollapseChange={setIsCollapsed} />
-
-        <main
-          className="mainArea"
-          style={{
-            marginLeft: isCollapsed ? "80px" : "250px",
-            // subtract topbar height
-            marginTop: "64px",
-            transition: "margin-left 0.3s ease",
-          }}
-        >
-          {children}
-        </main>
-      </div>
+      {children}
     </>
   );
 }
