8f579ce4157d2bb5552ce3af9e6520a912ef7663
ankitbareth-dev <ankitbareth.dev@gmail.com>
2025-11-29 13:39:01 +0530

    updated the top bar and removed the profile section

--- app/dashboard/components/Topbar/Topbar.tsx
+++ app/dashboard/components/Topbar/Topbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import styles from "./Topbar.module.sass";
-import { FiChevronDown, FiMoon, FiSun } from "react-icons/fi";
+import { FiMoon, FiSun } from "react-icons/fi";
 import { useTheme } from "next-themes";
 import { useEffect, useRef } from "react";
 import ClockIcon from "./ClockIcon";
@@ -34,19 +34,8 @@ export default function Topbar() {
             )
           ) : (
             <FiMoon />
-          )}{" "}
-          {/* fallback icon so UI doesn't shift */}
+          )}
         </button>
-
-        <div className={styles.user}>
-          <img
-            src="https://i.pravatar.cc/300"
-            alt="user"
-            className={styles.avatar}
-          />
-          <span className={styles.name}>Himanshu</span>
-          <FiChevronDown className={styles.arrow} />
-        </div>
       </div>
     </header>
   );
